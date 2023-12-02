6d80b1d0585d076b2704398eeee87b32c8d091f4
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-12-02 14:45:58 -0300

    feat: adding form validations and flash messages

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -13,4 +13,29 @@ class UserController
         $user = findBy('users', 'id', $request['user'], 'email, name');
         dd($user);
     }
+
+    public function create()
+    {
+        return [
+            'view' => 'User/create_template.php',
+            'data' => [
+                'title' => 'Create User',
+            ],
+        ];
+    }
+
+    public function store()
+    {
+        $validate = validate([
+            'name' => 'required',
+            'email' => 'email|unique:users',
+            'password' => 'required|minlen:3',
+        ]);
+
+        if (!$validate) {
+            return redirect('/user/create');
+        }
+
+        dd($validate);
+    }
 }
