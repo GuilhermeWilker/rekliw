339529b31590050d5d288a9a3660eab7bc35b369
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-12-03 13:14:34 -0300

    refactor: changing route calls

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    /*
     'GET' => [
         '/' => 'HomeController@index',
         '/user/create' => 'UserController@create',
@@ -12,4 +13,13 @@ return [
         '/login' => 'LoginController@store',
         '/user/store' => 'UserController@store',
     ],
+*/
+    ['GET', '/', 'HomeController@index'],
+    ['GET', '/user/create', 'UserController@create'],
+    ['GET', '/user/[0-9]+', 'UserController@show'],
+    ['GET', '/login', 'LoginController@index'],
+    ['GET', '/logout', 'LoginController@destroy'],
+
+    ['POST', '/login', 'LoginController@store'],
+    ['POST', '/user/store', 'UserController@store'],
 ];
